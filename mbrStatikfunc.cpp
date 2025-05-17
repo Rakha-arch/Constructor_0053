b4aec910c9f7afdf9b43c4d8c6661515d071a27d
Rakha-arch <miftahuzahran@gmail.com>
2025-05-17 14:39:08 +0700

    commit18

---
+++ mbrStatikfunc.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+    static int nim;
+    int id;
+    string nama;
+public:
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; } // Definisi Function
+    static int getNim() { return nim; }         // Definisi Function
+
+    mahasiswa(string pnama) : nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+   
+}
+
+void mahasiswa::printAll() {
+   
+}
+
+int main() {
+ 
+}
