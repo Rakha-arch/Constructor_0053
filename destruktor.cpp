77d7cb8647c17bfdb03dbe0a5682b8fd325fa946
Rakha-arch <miftahuzahran@gmail.com>
2025-05-17 14:27:35 +0700

    commit12

--- destruktor.cpp
+++ destruktor.cpp
@@ -44,5 +44,8 @@ void angka::isiData()
 }
 int main()
 {
-   
+    angka belajarcpp(3); //constructor dipanggil
+    angka *ptrBelajarcpp = new angka(5); //constructor dipanggil
+    delete ptrBelajarcpp; //destructor dipanggil
+    return 0;
 }//distructor dipanggil
