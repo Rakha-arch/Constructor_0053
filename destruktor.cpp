d09b74bdd65b2b1ebf6b849bcb6c2892025eb9a5
Rakha-arch <miftahuzahran@gmail.com>
2025-05-17 14:23:23 +0700

    commit6

---
+++ destruktor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class angka
+{
+private:
+    int *arr;
+    int panjang;
+public:
+    angka(int); //constructor
+    angka(); //destructor
+    void cetakData();
+    void isiData();
+};
+
+{  
+}
+
+angka::angka()
+{ //destructor
+   
+}
+
+void angka::cetakData()
+{
+    
+}
+
+void angka::isiData()
+{
+   
+}
+int main()
+{
+   
+}//distructor dipanggil
